2a9179302fe0a41ca2798dca78b68a635c5f71a6
Virac David AUK <auk.david@gmail.com>
2021-04-29 14:10:44 +0200

    Move to HTTP + Favicon

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const path = require('path')
 const https = require('https')
 const express = require('express')
 
-const PORT = 443
+const PORT = 12103
 const app = express()
-const server = https.createServer({
-  key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
-  cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem'))
-}, app)
+// const server = https.createServer({
+//   key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
+//   cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem'))
+// }, app)
 
 app.use((req, res, next) => {
   const value = Object.keys(req.query).join(',')
@@ -19,4 +19,4 @@ app.use((req, res, next) => {
 
 app.use(express.static('public'))
 
-server.listen(PORT, () => console.log(`Listening https://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`Listening http://localhost:${PORT}`))
