2c65591c6256f68d28a4c267a9a810937702577f
Virac David AUK <auk.david@gmail.com>
2021-04-29 16:12:45 +0200

    Cleanup

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-const fs = require('fs')
-const path = require('path')
-const https = require('https')
 const express = require('express')
 
 const PORT = 12103
 const app = express()
-// const server = https.createServer({
-//   key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
-//   cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem'))
-// }, app)
 
 app.use((req, res, next) => {
-  const value = Object.keys(req.query).join(',')
-  console.log(value)
-  res.header("Accept-CH", value);
+  res.header("Accept-CH", Object.keys(req.query).join(','));
   next();
- });
+});
 
 app.use(express.static('public'))
 
